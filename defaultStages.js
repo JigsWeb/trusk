50c6602fe94f50b42867a610b1065f45b5929b1a
Anthony <anthony@bob-desk.fr>
2020-08-18 10:21:51 +0100

    Review fixes

--- defaultStages.js
+++ defaultStages.js
@@ -1,33 +1,45 @@
+import { filter } from "lodash";
+
+const errorFunctions = {
+  input: value => new Promise((resolve, reject) => value.length ? resolve() : reject('Réponse vide invalide (chaîne de caractères attendue)')),
+  number: value => new Promise((resolve, reject) => value && Number.isInteger(value) ? resolve() : reject('Réponse invalide (nombre entier attendu)')),
+}
+
+const filterFunctions = {
+  input: value => value.trim(),
+  number: value => Number.isInteger(parseInt(value)) ? parseInt(value) : value
+}
+
 export default [
   {
     inquirerPromptConfig: {
       type: 'input',
       name: 'value',
       message: 'Comment vous apellez-vous ? (ex: Jean Dupont)',
-      filter: value => value.trim()
+      filter: filterFunctions.input
     },
     key: 'name',
-    validate: value => new Promise((resolve, reject) => value.length ? resolve() : reject()),
+    validate: errorFunctions.input,
   },
   {
     inquirerPromptConfig: {
       type: 'input',
       name: 'value',
       message: 'Quel est le nom de votre société ? (ex: Carrefour)',
-      filter: value => value.trim()
+      filter: filterFunctions.input
     },
     key: 'companyName',
-    validate: value => new Promise((resolve, reject) => value.length ? resolve() : reject()),
+    validate: errorFunctions.input,
   },
   {
     inquirerPromptConfig: {
       type: 'input',
       name: 'value',
       message: 'Combien d\'employé(e)s avez-vous ?',
-      filter: value => parseInt(value)
+      filter: filterFunctions.number
     },
     key: 'employeesCount',
-    validate: value => new Promise((resolve, reject) => value && Number.isInteger(value) ? resolve() : reject()),
+    validate: errorFunctions.number,
     isCount: true,
     inquirerArrayPromptConfig: [
       {
@@ -35,10 +47,10 @@ export default [
           type: 'input',
           name: 'value',
           message: 'Employé(e) $ - Comment s\'apelle t-il ?',
-          filter: value => value.trim()
+          filter: filterFunctions.input
         },
         key: 'employees.$.name',
-        validate: value => new Promise((resolve, reject) => value.length ? resolve() : reject()),
+        validate: errorFunctions.input,
       },
     ]
   },
@@ -47,10 +59,10 @@ export default [
       type: 'input',
       name: 'value',
       message: 'Combien de camion avez-vous ?',
-      filter: value => parseInt(value)
+      filter: filterFunctions.number
     },
     key: 'trucksCount',
-    validate: value => new Promise((resolve, reject) => value && Number.isInteger(value) ? resolve() : reject()),
+    validate: errorFunctions.number,
     isCount: true,
     inquirerArrayPromptConfig: [
       {
@@ -58,10 +70,10 @@ export default [
           type: 'input',
           name: 'value',
           message: 'Camion $ - Quel est son volume en m3 ?',
-          filter: value => parseInt(value)
+          filter: filterFunctions.number
         },
         key: 'trucks.$.volume',
-        validate: value => new Promise((resolve, reject) => value && Number.isInteger(value) ? resolve() : reject()),
+        validate: errorFunctions.number,
       },
       {
         inquirerPromptConfig: {
--- index.js
+++ index.js
@@ -18,6 +18,10 @@ function saveTrusker(trusker) {
   return new Promise(resolve => redis.set('trusker', JSON.stringify(trusker)).then(resolve))
 }
 
+function resetTrusker() {
+  return saveTrusker({});
+}
+
 function formatStageWithIndex(stage, i) {
   return {
     ...stage,
@@ -29,8 +33,6 @@ function formatStageWithIndex(stage, i) {
 async function restart() {
   stages = defaultStages;
 
-  await saveTrusker({});
-
   computeStage();
 }
 
@@ -39,15 +41,18 @@ async function displayInformations(trusker) {
 
   stages.forEach(stage => console.log(stage.inquirerPromptConfig.message, get(trusker, stage.key)))
 
-  const { isValid } = await inquirer.prompt([{ message: 'Les informations sont elles valides?', type: 'list', choices: ['Oui', 'Non'], name: 'isValid' }])
+  const { isValid } = await inquirer.prompt([{ message: 'Les informations sont elles valides?', type: 'confirm', name: 'isValid' }])
+
+  await resetTrusker();
 
   if (!isValid)
     return restart();
   
   console.log('\nMerci pour votre patience, nous reviendrons vers vous bientot');
+  process.exit();
 }
 
-async function computeStage(stageIndex = 0, trusker = null) {
+async function computeStage(stageIndex = 0, trusker = null, error = false) {
   const next = () => stages[++stageIndex] ? computeStage(stageIndex, trusker) : displayInformations(trusker);
   // Add a stages pipeline for each new employee/truck, can be a memory issue
   const pushNewStages = length => stages.splice(stageIndex + 1, 0, ...Array.from({ length }).flatMap((_, i) => stages[stageIndex].inquirerArrayPromptConfig.map(stage => formatStageWithIndex(stage, i)))); // magic ?
@@ -58,6 +63,8 @@ async function computeStage(stageIndex = 0, trusker = null) {
   const stage = stages[stageIndex];
 
   if (get(trusker, stage.key)) {
+    console.log(stage.inquirerPromptConfig.message, get(trusker, stage.key))
+    
     if (stage.isCount)
       pushNewStages(get(trusker, stage.key));
 
@@ -80,7 +87,8 @@ async function computeStage(stageIndex = 0, trusker = null) {
       
       return next();
     } catch (error) {
-      return computeStage(stageIndex, trusker);
+      console.log(`Erreur: ${error}`);
+      return computeStage(stageIndex, trusker, true);
     }
   } catch (error) {
     console.log(error)
